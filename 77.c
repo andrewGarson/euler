155fc20aca46e9e2c2bab8cebf2c8628cc98b5d5
Andrew Garson <andrew.garson@gmail.com>
2019-09-12 07:54:07 -0400

    WIP 77

--- 77.c
+++ 77.c
@@ -4,6 +4,20 @@
 #include "lib/list.h"
 #include "lib/primes.h"
 
+/*
+ * It is possible to write ten as the sum of primes 
+ * in exactly five different ways:
+ *
+ * 7 + 3
+ * 5 + 5
+ * 5 + 3 + 2
+ * 3 + 3 + 2 + 2
+ * 2 + 2 + 2 + 2 + 2
+ *
+ * What is the first value which can be written as 
+ * the sum of primes in over five thousand different ways?
+ */
+
 
 int *ways;
 void expand_ways(int new_target){
@@ -23,60 +37,83 @@ int found_5000(int target) {
   return -1;
 }
 
+int expand_primes(int biggest_prime, List *primes) {
+  int *next = calloc(1, sizeof(int));
+  *next = next_prime(biggest_prime);
+  printf("expand from: %d\n", biggest_prime);
+  printf("next: %d\n", *next);
+  list_insert_back(primes, next); 
+  printf("next prime inserted\n\n");
+  return *next;
+}
+
 int main() {
 
-  int target = 64;
-  ways = calloc(target + 1, sizeof(int));
-  ways[0] = 1;
-  ways[1] = 0;
+  int target = 2;
+  expand_ways(target);
+  ways[0] = 1; // there is exactly 1 way to make 0 -- use no primes at all
+  ways[1] = 0; // there is no way to make 1
 
   List *primes = list_create();
 
   int biggest_prime = 1;
-  // keep looking until we find a number with over 5000 ways to make it
-  while(found_5000(target) == -1){
-    
-    // extend out list of primes until our biggest prime is too big to be useful
-    // this happens when it is > our current target
-    //
-    // if our biggest prime is too big to use, then we need to increase our target
-    // we need to fill in the ways to calculate these new targets using each of the
-    // primes we had previously used
-    //
-    biggest_prime = next_prime(biggest_prime);
-    int *copy = calloc(1, sizeof(int));
-    *copy = biggest_prime;
-    list_insert_back(primes, (void *)copy); 
-    printf("Using Prime = %d\n", biggest_prime);
 
+  while(ways[target] < 5000) {
+    biggest_prime = expand_primes(biggest_prime, primes);
+    while(biggest_prime <= target) {
+      ListNode *current_prime = primes->head;
+      while(current_prime) {
+        int prime = *(int *)(current_prime->value);
+        for(int n = biggest_prime; n <= target; n++) {
 
-    if(biggest_prime > target) {
-      // increase target, expand the ways array accordingly
-      // starting at old_target + 1, use each prime to calculate ways for
-      // that prime
+          printf("biggestPrime: %d\n", biggest_prime);
+          printf("prime: %d\n", prime);
+          printf("target: %d\n", target);
+          printf("n: %d\n", target);
+          printf("\n");
+          printf("ways[%d]: %d\n", n, ways[n]);
+          printf("ways[n-prime]: %d\n", ways[n-prime]);
+          
+          ways[n] += ways[n - prime];
+          if(ways[n] >= 5000) {
+            printf("biggest prime: %d\n", biggest_prime);
+            printf("N: %d\n", n);
+            exit(0);
+          }
+          printf("\n\n");
+        }
+        current_prime = current_prime->next;
+      }
+      biggest_prime = expand_primes(biggest_prime, primes);
     }
 
+    printf("Need to expand target\n\n");
+    target++;
+    expand_ways(target);
+    while(target <= biggest_prime) {
+
+      ListNode *current_prime = primes->head;
+      while(current_prime) {
+        int prime = *(int *)current_prime->value;
+        ways[target] += ways[target - prime]; 
+        current_prime = current_prime->next;
+      }
+
+      if(ways[target] >= 5000) {
+        printf("Target: %d\n", target);
+      }
+      
+      target++;
+      expand_ways(target);
+    }
+
+  
   }
 
-  int index = found_5000(target);
-  printf("Found %d ways to make %d\n", ways[index], index);
+  printf("Found %d ways to make %d\n", ways[target], target);
 
   return 0;
 }
-
-/*
- * It is possible to write ten as the sum of primes 
- * in exactly five different ways:
- *
- * 7 + 3
- * 5 + 5
- * 5 + 3 + 2
- * 3 + 3 + 2 + 2
- * 2 + 2 + 2 + 2 + 2
- *
- * What is the first value which can be written as 
- * the sum of primes in over five thousand different ways?
- */
 
 /*
   int main() {
